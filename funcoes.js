5e7bde1a0e25392ce3bb8115d89520281fff5a6d
José Yoshiriro <jyoshiriro@gmail.com>
2026-08-12 18:00:56 -0300

    transacoes sem var global

--- funcoes.js
+++ funcoes.js
@@ -1,10 +1,5 @@
-
 async function fetchBalance() {
   const response = await fetch('https://cofrinhotadashi-nodejs-serverless-f.vercel.app/api/balance');
   const data = await response.json();
   return data;
-}
-
-async function main() {
-  await fetchBalance();
 }
